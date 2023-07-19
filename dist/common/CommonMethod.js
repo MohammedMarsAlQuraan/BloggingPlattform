1b36db5ba83f9f8b9eb2976460b54123983c0094
MohammedMarsAlQuraan <mohammedmarsalquraan@gmail.com>
2023-07-19 22:52:52 +0300

    Final commit

--- dist/common/CommonMethod.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CommonMethod = void 0;
-class CommonMethod {
-    static isStringEmptyOrUndefiend(str) {
-        return str === null || str === undefined || str.trim() === '';
-    }
-}
-exports.CommonMethod = CommonMethod;
--- dist/database/models/sqlResult.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SQLResult = void 0;
-class SQLResult {
-}
-exports.SQLResult = SQLResult;
